5a7bd2bd8c83a914f3a9069976e24362d5c7c050
karam fikre <karam_fikre@hotmail.com>
2018-07-05 22:55:31 +0200

    Setup the Xunit Test for the project. Added the tests for BranchController.

---
+++ LibraryTest/Controllers/BranchControllerTest.cs
@@ -0,0 +1,107 @@
+﻿using Library.Controllers;
+using Library.Models.Branch;
+using LibraryData;
+using LibraryData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+namespace LibraryTest.Controllers
+{
+    public class BranchControllerTest
+    {
+        [Fact]
+        public void ReturnBranchIndexView()
+        {
+            var mockBranchService = new Mock<ILibraryBranch>();
+            mockBranchService.Setup(r => r.GetAll()).Returns(GetAllBranches());
+            //Arrange
+            var controller = new BranchController(mockBranchService.Object);
+
+            //Act
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void ReturnBranchIndexModel()
+        {
+            var mockBranchService = new Mock<ILibraryBranch>();
+            mockBranchService.Setup(r => r.GetAll()).Returns(GetAllBranches());
+            //Arrange
+            var controller = new BranchController(mockBranchService.Object);
+
+            var result = controller.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsType<BranchIndexModel>(viewResult.Model);
+
+        }
+
+        [Fact]
+        public void ReturnBranchDetailView()
+        {
+            var mockBranchService = new Mock<ILibraryBranch>();
+            mockBranchService.Setup(r => r.Get(888)).Returns(GetBranch());
+
+            var controller = new BranchController(mockBranchService.Object);
+
+            var result = controller.Detail(888);
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void ReturnBranchDetailModel()
+        {
+            var mockBranchServices =new Mock<ILibraryBranch>();
+            mockBranchServices.Setup(r => r.Get(888)).Returns(GetBranch());
+
+            var controller = new BranchController(mockBranchServices.Object);
+
+            var result = controller.Detail(888);
+            var resultView = Assert.IsType<ViewResult>(result);
+
+            Assert.IsType<BranchDetailModel>(resultView.Model);
+        }
+
+        private static IEnumerable<LibraryBranch> GetAllBranches()
+        {
+            return new List<LibraryBranch>()
+            {
+                new LibraryBranch
+                {
+                    Id = 123,
+                    Name = "Sequoia Branch",
+                    Address = "1 Main St"
+                },
+
+                new LibraryBranch
+                {
+                    Id = 431,
+                    Name = "Lake Branch",
+                    Address = "2 Oak Dr"
+                },
+
+                new LibraryBranch
+                {
+                    Id = 888,
+                    Name = "Park Branch",
+                    Address = "3 Commerce St"
+                }
+            };
+        }
+
+        private static LibraryBranch GetBranch()
+        {
+            return new LibraryBranch
+            {
+                Id = 888,
+                Name = "Park Branch",
+                Address = "3 Commerce St",
+                Telephone = "123",
+            };
+        }
+    }
+}
---
+++ LibraryTest/Controllers/CatalogControllerTest.cs
@@ -0,0 +1,140 @@
+﻿using Library.Controllers;
+using Library.Models.Branch;
+using Library.Models.Catalog;
+using LibraryData;
+using LibraryData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace LibraryTest.Controllers
+{
+    public class CatalogControllerTest
+    {
+        [Fact]
+        public void ReturnIndexViewAndModel()
+        {
+            var mockLibraryAssetServices = new Mock<ILibraryAsset>();
+            var mockCheckoutService = new Mock<ICheckout>();
+            mockLibraryAssetServices.Setup(r => r.GetAll()).Returns(GetAllAssets);
+
+            var controller = new CatalogController(mockLibraryAssetServices.Object, mockCheckoutService.Object);
+
+            var result = controller.Index();
+            var viewResult=Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<AssetIndexModel>(
+                 viewResult.ViewData.Model);
+            Assert.Equal(2, model.Assets.Count());
+
+        }
+
+        [Fact]
+        public void ReturnDetailViewAndModel()
+        {
+            var mockLibraryAssetService = new Mock<ILibraryAsset>();
+            var mockCheckoutService = new Mock<ICheckout>();
+            mockLibraryAssetService.Setup(r => r.GetById(24)).Returns(GetAsset());
+
+            mockCheckoutService.Setup(r => r.GetCurrentHoldPatronName(24)).Returns(GetCurrentHold().HoldPlaced);
+            mockCheckoutService.Setup(r => r.GetCurrentHoldPatronName(24)).Returns(GetCurrentHold().PatronName);
+
+            mockCheckoutService.Setup(r => r.GetCheckoutHistory(24)).Returns(new List<CheckoutHistory>
+            {
+                new CheckoutHistory()
+            });
+
+            mockLibraryAssetService.Setup(r => r.GetType(24)).Returns("Book");
+            mockLibraryAssetService.Setup(r => r.GetCurrentLocation(24)).Returns(new LibraryBranch
+            {
+                Name = "Hawkins Library"
+            });
+            mockLibraryAssetService.Setup(r => r.GetAuthorOrDirector(24)).Returns("Virginia Woolf");
+           
+            mockLibraryAssetService.Setup(r => r.GetDeweyIndex(24)).Returns("ELEVEN");
+            mockCheckoutService.Setup(r => r.GetCheckoutHistory(24)).Returns(new List<CheckoutHistory>
+            {
+                new CheckoutHistory()
+            });
+            mockCheckoutService.Setup(r => r.GetLatestCheckout(24)).Returns(new Checkout());
+            mockCheckoutService.Setup(r => r.GetCurrentHoldPatronName(24)).Returns("NANCY");
+            var sut = new CatalogController(mockLibraryAssetService.Object, mockCheckoutService.Object);
+
+            var result = sut.Detail(24);
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<AssetDetailModel>(
+                viewResult.ViewData.Model);
+            Assert.Equal("Orlando", model.Title);
+
+
+        }
+
+
+
+
+        private static IEnumerable<LibraryAsset> GetAllAssets()
+        {
+            return new List<LibraryAsset>()
+            {
+                new Book
+                {
+                    Title = "Orlando",
+                    Author = "Virginia Woolf",
+                    Year = 1928,
+                    Cost = 23.0M,
+                    ImageUrl = "foo",
+                    Status = new Status
+                    {
+                        Name = "Checked In",
+                        Id = 1
+                    }
+                },
+
+                new Video
+                {
+                    Title = "Happy People",
+                    Director = "Werner Herzog",
+                    ImageUrl = "images/sample.jpg",
+                    Status = new Status
+                    {
+                        Name = "Lost",
+                        Id = 3
+                    }
+                }
+            };
+        }
+
+        private static Book GetAsset()
+        {
+            return new Book
+            {
+                Title = "Orlando",
+                Author = "Virginia Woolf",
+                Status = new Status
+                {
+                    Name = "Checked In",
+                    Id = 1
+                }
+            };
+        }
+
+        private static AssetHoldModel GetCurrentHold()
+        {
+            return new AssetHoldModel
+            {
+                PatronName = "Foo",
+                HoldPlaced = "Bar"
+            };
+        }
+    }
+
+}
+
+
+
+
+
+
+
+    
